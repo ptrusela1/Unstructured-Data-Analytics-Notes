6a647666b1d1925da883c063f01972c80c613ed3
ptrusela1 <ptrusela@nd.edu>
2025-01-27 14:11:42 -0500

    gba_lyrics

--- unstructured_lyrics_notes.py
+++ unstructured_lyrics_notes.py
@@ -5,3 +5,10 @@ import pyarrow
 lyrics_pd = pd.read_feather(
   'C:/Users/pauly/OneDrive/Documents/complete_lyrics_2025.feather'
 )
+
+gba = lyrics_pd.iloc[127]
+
+gba_lyrics = gba['lyrics']
+
+gba_lyrics
+
